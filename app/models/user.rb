800b72e07bfaf6e1173b15e26b3bd0cd4c335daf
diegoarvz4 <darvz.gtz@gmail.com>
2019-07-05 11:41:54 -0500

    Added relationships

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    has_many :posts
+    has_many :comments
+    validates :username, 
+                    presence: true, 
+                    uniqueness: true 
+end
